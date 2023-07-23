b437226dfb30f64f270f3b39bfd0e0ec20b60013
Rubens Rojas <rubensarojas@gmail.com>
2023-07-23 20:18:36 -0300

    Adding more customization for the Modal

--- components/modal.tsx
+++ components/modal.tsx
@@ -1,10 +1,18 @@
+import { twMerge } from "tailwind-merge";
+
 const Modal: React.FC<{
+  className?: string;
   children: React.ReactNode;
   closeModal: () => void;
-}> = ({ closeModal, children }) => {
+}> = ({ closeModal, children, className }) => {
   return (
-    <div className="absolute top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-25 animate px-2.5 backdrop-blur-sm">
-      <div className="relative flex flex-col gap-8 sm:gap-16 px-6 py-4 bg-white rounded border-2 border-black w-full max-w-2xl lg:max-w-4xl shadow-sm">
+    <div
+      className={twMerge(
+        "flex items-center justify-center absolute top-0 left-0 w-full min-h-screen h-max bg-black bg-opacity-25 animate py-10 px-2.5 backdrop-blur-sm overflow-auto",
+        className
+      )}
+    >
+      <div className="relative flex flex-col gap-8 sm:gap-16 px-6 py-4 bg-white rounded border-2 border-black w-full sm:w-fit max-w-2xl md:max-w-4xl shadow-sm">
         <button
           className="absolute right-0 top-0 block w-fit underline leading-8 h-fit mr-6 mt-4"
           type="button"
