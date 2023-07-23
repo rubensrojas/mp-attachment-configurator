e77cd6c245c2606e4cc026294e9b1234969f8bbc
Rubens Rojas <rubensarojas@gmail.com>
2023-07-23 04:14:47 -0300

    Breaker page mockup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,60 @@
+import Image from "next/image";
+
+const perks = ["Expert advice", "Fast & easy financing", "24/7 support"];
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="px-20 pt-44 max-w-xxl mx-auto">
+      <div className="flex flex-col gap-16 md:flex-row max-w-[1288px] mx-auto">
+        <div className="flex flex-col gap-8 max-w-[680px]">
+          <h1 className="text-6xl font-bold">New Breakers For Sale</h1>
+          <p className=" text-light-gray leading-snug">
+            We have a full range of the best and most affordable excavator
+            breakers. Perfect for processing aggregates like concrete, asphalt,
+            gravel and more. Our wide range of equipment is designed to support
+            everyone and every budget, from small to growing businesses. We also
+            offer a large range of{" "}
+            <a href="https://www.machinerypartner.com/heavy-equipment-categories/crushers">
+              crushers
+            </a>
+            ,{" "}
+            <a href="https://www.machinerypartner.com/heavy-equipment-categories/screeners">
+              screeners
+            </a>
+            , and{" "}
+            <a href="https://www.machinerypartner.com/heavy-equipment-categories/conveyors">
+              conveyors
+            </a>
+            .
+          </p>
+          <div className="flex gap-8">
+            {perks.map((perk) => (
+              <div key={perk} className="flex gap-2">
+                <Image
+                  src="/check-mark.svg"
+                  alt="Check"
+                  width={24}
+                  height={24}
+                />
+                <p className="font-semibold">{perk}</p>
+              </div>
+            ))}
+          </div>
+          <button
+            type="button"
+            className="py-4 px-6 w-fit block bg-brand-yellow rounded font-semibold leading-none text-lg"
+          >
+            Talk to an expert today
+          </button>
+        </div>
+        <Image
+          className="object-contain h-auto max-h-[330px]"
+          src="/breaker.png"
+          alt="Breaker"
+          width={540}
+          height={330}
+        />
+      </div>
+    </main>
+  );
 }
